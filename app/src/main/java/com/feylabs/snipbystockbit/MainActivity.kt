9b61f8cc1b988b8846a9f676a621b8b137614a92
Henry Augusta <henryaugusta4@gmail.com>
2023-04-27 15:03:11 +0700

    ✨ Create New Modules : shared-dependencies (base dependencies) and feature module named feature-example

--- app/src/main/java/com/feylabs/snipbystockbit/MainActivity.kt
+++ app/src/main/java/com/feylabs/snipbystockbit/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.feylabs.snipbystockbit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.feylabs.core.helper.toast.showToast
+import com.feylabs.core.helper.toast.ToastHelper.showToast
+import com.feylabs.feature_example.FeatureExampleMainActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         showToast("Hai Apa Kabar")
+
+        startActivity(Intent(this, FeatureExampleMainActivity::class.java))
     }
 }
---
+++ shared-dependencies/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.feylabs.shared_dependencies"
+    compileSdk = 32
+
+    defaultConfig {
+        minSdk = 26
+        targetSdk = 32
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Kotlin Coroutines for asynchronous programming
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+
+    // Dagger Hilt for dependency injection
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+
+    // Fragment KTX for simplifying Fragment development
+    implementation("androidx.fragment:fragment-ktx:1.4.1")
+
+    // Room Dependencies
+    implementation("androidx.room:room-runtime:2.4.2")
+    implementation("androidx.room:room-ktx:2.4.2")
+    kapt("androidx.room:room-compiler:2.4.2")
+
+    // Picasso for loading and caching images
+    implementation("com.squareup.picasso:picasso:2.8")
+
+    // Retrofit for making implementation requests
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // Gson converter for Retrofit to handle JSON response data
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    // OkHttp for handling HTTP requests and caching
+    implementation("com.squareup.okhttp3:okhttp:4.9.2")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.1.1")
+}
