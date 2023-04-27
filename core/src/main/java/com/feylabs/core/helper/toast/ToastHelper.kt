65552a3252519e66ed5152070c700a0c69e5ce59
Henry Augusta <henryaugusta4@gmail.com>
2023-04-27 15:00:31 +0700

    ♻ Refactor Helper Toast

--- core/src/main/java/com/feylabs/core/helper/toast/ToastHelper.kt
+++ core/src/main/java/com/feylabs/core/helper/toast/ToastHelper.kt
@@ -25,40 +25,42 @@ object ToastHelper {
     fun showToastLong(context: Context, message: String) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     }
-}
 
-/**
- * Displays a short duration Toast message with the given message text.
- *
- * @param message the message to display.
- */
-fun Activity.showToast(message: String) {
-    ToastHelper.showToast(this, message)
-}
 
-/**
- * Displays a long duration Toast message with the given message text.
- *
- * @param message the message to display.
- */
-fun Activity.showToastLong(message: String) {
-    ToastHelper.showToastLong(this, message)
-}
+    /**
+     * Displays a short duration Toast message with the given message text.
+     *
+     * @param message the message to display.
+     */
+    fun Activity.showToast(message: String) {
+        ToastHelper.showToast(this, message)
+    }
 
-/**
- * Displays a short duration Toast message with the given message text.
- *
- * @param message the message to display.
- */
-fun Fragment.showToast(message: String) {
-    ToastHelper.showToast(requireContext(), message)
-}
+    /**
+     * Displays a long duration Toast message with the given message text.
+     *
+     * @param message the message to display.
+     */
+    fun Activity.showToastLong(message: String) {
+        ToastHelper.showToastLong(this, message)
+    }
 
-/**
- * Displays a long duration Toast message with the given message text.
- *
- * @param message the message to display.
- */
-fun Fragment.showToastLong(message: String) {
-    ToastHelper.showToastLong(requireContext(), message)
+    /**
+     * Displays a short duration Toast message with the given message text.
+     *
+     * @param message the message to display.
+     */
+    fun Fragment.showToast(message: String) {
+        ToastHelper.showToast(requireContext(), message)
+    }
+
+    /**
+     * Displays a long duration Toast message with the given message text.
+     *
+     * @param message the message to display.
+     */
+    fun Fragment.showToastLong(message: String) {
+        ToastHelper.showToastLong(requireContext(), message)
+    }
+
 }
