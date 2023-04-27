de6a3d27dfded236d0776362ec5ed36246ffdcf3
Henry Augusta <henryaugusta4@gmail.com>
2023-04-27 22:43:29 +0700

    ♻ Move Feature Example into submodules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
 
     implementation(project(":core"))
     implementation(project(":shared-dependencies"))
-    implementation(project(":feature-example"))
+    implementation(project(":feature:feature-example"))
 
     // Navigation
     val navigation_version = "2.3.4"
--- feature/feature-example/src/main/java/com/feylabs/feature_example/FeatureExampleMainActivity.kt
+++ feature/feature-example/src/main/java/com/feylabs/feature_example/FeatureExampleMainActivity.kt
@@ -1,10 +1,8 @@
 package com.feylabs.feature_example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.feylabs.core.base.BaseActivity
 import com.feylabs.core.helper.snackbar.SnackbarHelper.showSnackbar
-import com.feylabs.core.helper.toast.ToastHelper.showToast
 import com.feylabs.feature_example.databinding.ActivityFeatureExampleMainBinding
 
 class FeatureExampleMainActivity : BaseActivity<ActivityFeatureExampleMainBinding>() {
