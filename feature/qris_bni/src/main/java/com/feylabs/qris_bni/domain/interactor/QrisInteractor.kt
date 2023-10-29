761d6fdc4eab41e9ff187b58c95bb69fe61f8c68
Henry Augusta <henryaugusta4@gmail.com>
2023-10-29 15:21:27 +0700

    QRIS BNI : add interactor and use case on domain.

---
+++ feature/qris_bni/src/main/java/com/feylabs/qris_bni/domain/interactor/QrisInteractor.kt
@@ -0,0 +1,26 @@
+package com.feylabs.qris_bni.domain.interactor
+
+import com.feylabs.core.helper.wrapper.ResponseState
+import com.feylabs.qris_bni.domain.uimodel.TransactionUiModel
+import com.feylabs.qris_bni.domain.usecase.QrUseCase
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class QrisInteractor @Inject constructor(
+    private val qrRepository: QrisInteractor
+) : QrUseCase {
+
+    override suspend fun getAllTransaction(): Flow<ResponseState<List<TransactionUiModel>>> {
+        return qrRepository.getAllTransaction()
+    }
+
+    override suspend fun addTransaction(
+        merchantName: String,
+        transactionAmount: Double,
+        transactionType: String,
+        timestamp: Long
+    ): Flow<ResponseState<String>> {
+        return qrRepository.addTransaction(merchantName, transactionAmount, transactionType, timestamp)
+    }
+
+}
---
+++ feature/qris_bni/src/main/java/com/feylabs/qris_bni/domain/usecase/QrUseCase.kt
@@ -0,0 +1,18 @@
+package com.feylabs.qris_bni.domain.usecase
+
+import com.feylabs.core.helper.wrapper.ResponseState
+import com.feylabs.qris_bni.domain.uimodel.TransactionUiModel
+import kotlinx.coroutines.flow.Flow
+
+interface QrUseCase {
+    suspend fun getAllTransaction(
+    ): Flow<ResponseState<List<TransactionUiModel>>>
+
+    suspend fun addTransaction(
+        merchantName:String,
+        transactionAmount:Double,
+        transactionType:String,
+        timestamp:Long
+    ): Flow<ResponseState<String>>
+
+}
